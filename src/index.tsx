ba3701a872809219243ab7babb7ac140356c4f51
ToKimSeoYeon <rlagmlwnd3@daum.net>
2019-04-15 22:53:16 +0900

    apollo setup part one

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { ApolloProvider } from "react-apollo";
 import ReactDOM from "react-dom";
+import client from "./apollo";
 import App from "./App";
-import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
+);
